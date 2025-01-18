1ca8be2925fdf79d39be882ff1043d6d9d8151d5
Naoki427 <156777871+Naoki427@users.noreply.github.com>
2025-01-19 07:58:00 +0900

    update mariadb

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 include ./srcs/.env
-export $(shell sed 's/=.*//' .env)
+# export $(shell sed 's/=.*//' ./srcs/.env)
 all: up
 
 up: 
 	mkdir -p $(WP_VOLUME)
 	mkdir -p $(DB_VOLUME)
-	sudo chmod -R 755 $(WP_VOLUME)
-	sudo chmod -R 755 $(DB_VOLUME)
+	chmod -R 755 $(WP_VOLUME)
+	chmod -R 755 $(DB_VOLUME)
 	docker compose -f srcs/docker-compose.yml up
 
 build:
--- srcs/requirements/mariadb/tools/mdb-conf.sh
+++ srcs/requirements/mariadb/tools/mdb-conf.sh
@@ -4,20 +4,12 @@ service mariadb start
 sleep 5
 
 mysql -uroot -p"$MYSQL_PASSWORD" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}'; FLUSH PRIVILEGES;"
-
-# mariadb -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB}\`;"
-
-# mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'127.0.0.1' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-
-# mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-
-
 mysql -uroot -p"$MYSQL_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DB}\`;"
 mysql -uroot -p"$MYSQL_PASSWORD" -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'127.0.0.1' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mysql -uroot -p"$MYSQL_PASSWORD" -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mysql -uroot -p"$MYSQL_PASSWORD" -e "FLUSH PRIVILEGES;"
 
-mariadb -e "FLUSH PRIVILEGES;"
+mariadb     "FLUSH PRIVILEGES;"
 
 mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
 
